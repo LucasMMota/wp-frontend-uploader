c8485aab7155492fad635108f56bee42068f5725
lucas_mmfonseca <lucas.lmmf@gmail.com>
2017-08-09 15:19:26 -0300

    Add button on post types permissed by fu settings

--- lib/php/class-frontend-uploader-wp-visual-form.php
+++ lib/php/class-frontend-uploader-wp-visual-form.php
@@ -12,6 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class FU_WP_Visual_Form {
 
+	const settings_slug = 'frontend_uploader_settings';
+
 	public function __construct() {
 		add_action( 'admin_enqueue_scripts', array( $this, 'fu_visual_form_add_scripts' ) );
 		//add_action( 'media_buttons', array( $this, 'add_btn_fu_visual_form' ), 999 );
@@ -28,31 +30,47 @@ class FU_WP_Visual_Form {
 	 * Displays visual form button on post editor
 	 */
 	public static function add_btn_fu_visual_form() {
-		$current             = get_current_screen();
-		$arrPostTypesAllowed = array( 'post', 'page' );
+		$current = get_current_screen();
+		$arrDefaultPostTypesAllowed = array( 'post' );
+
+		$fuSettings                 = get_option( FU_WP_Visual_Form::settings_slug, FU_WP_Visual_Form::settings_defaults() );
+		if ( isset( $fuSettings['enabled_post_types'] ) ) {
+			$arrDefaultPostTypesAllowed = $fuSettings['enabled_post_types'];
+		}
 
 		/**
 		 * Filter to customize button exhibition displaying on custom post types
 		 */
-		$arrPostTypesAllowed = apply_filters( 'fu_visual_form_button_allowed_post_types', $arrPostTypesAllowed );
+		$arrPostTypesAllowed = apply_filters( 'fu_visual_form_button_allowed_post_types', $arrDefaultPostTypesAllowed );
 		if ( in_array( $current->id, $arrPostTypesAllowed, true ) ) {
 			?>
             <a href="/?TB_inline&inlineId=fu-btn-visual-form&width=500&height=400"
                class="button add_media hide-if-no-js thickbox"><span class="dashicons dashicons-welcome-widgets-menus"></span> Add FU Form </a>
 
-<!--            <input type="hidden" id="abril-shortcode-btn-votar-finalista-id">-->
-<!--            <input type="hidden" id="abril-shortcode-btn-votar-premio-id">-->
+            <!--            <input type="hidden" id="abril-shortcode-btn-votar-finalista-id">-->
+            <!--            <input type="hidden" id="abril-shortcode-btn-votar-premio-id">-->
 
             <div id="fu-btn-visual-form" style="display:none;">
                 <div class="fu-btn-visual-form">
                     <div class="edit-shortcode-form">
-<!--                        <p>Preencha o texto que deseja exibir.</p>-->
-<!--                        <label>Texto do botão</label>-->
-<!--                        <input id="abril-shortcode-btn-votar-texto" class="abril-shortcode-btn-votar-texto" value="Gostou desta finalista?">-->
-<!---->
-<!--                        <div class="abril-bv-btn-insert-container">-->
-<!--                            <button class="button button-primary button-large" id="abril-bv-inserir">Inserir Botão</button>-->
-<!--                        </div>-->
+                        SCTDs
+
+                        fu-upload-form abre
+                        fu-upload-response
+
+                        input
+                        textarea
+                        select
+                        checkboxes
+                        radio
+                        recaptcha
+                        <!--                        <p>Preencha o texto que deseja exibir.</p>-->
+                        <!--                        <label>Texto do botão</label>-->
+                        <!--                        <input id="abril-shortcode-btn-votar-texto" class="abril-shortcode-btn-votar-texto" value="Gostou desta finalista?">-->
+                        <!---->
+                        <!--                        <div class="abril-bv-btn-insert-container">-->
+                        <!--                            <button class="button button-primary button-large" id="abril-bv-inserir">Inserir Botão</button>-->
+                        <!--                        </div>-->
                     </div>
                 </div>
             </div>
@@ -60,6 +78,20 @@ class FU_WP_Visual_Form {
 		}
 	}
 
+	/**
+	 * Ensure we're not producing any notices by supplying the defaults to get_option
+	 *
+	 * @return array $defaults
+	 */
+	private static function settings_defaults() {
+		$defaults = array();
+		$settings = Frontend_Uploader_Settings::get_settings_fields();
+		foreach ( $settings[ FU_WP_Visual_Form::settings_slug ] as $setting ) {
+			$defaults[ $setting['name'] ] = $setting['default'];
+		}
+
+		return $defaults;
+	}
 }
 
 // Trigger
