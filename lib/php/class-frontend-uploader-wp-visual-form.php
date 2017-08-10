eb179697402f7eb314125f0ef68da71fb9e3c6a0
lucas_mmfonseca <lucas.lmmf@gmail.com>
2017-08-10 14:08:48 -0300

    Add tabs

--- lib/php/class-frontend-uploader-wp-visual-form.php
+++ lib/php/class-frontend-uploader-wp-visual-form.php
@@ -20,20 +20,16 @@ class FU_WP_Visual_Form {
 	}
 
 	public function fu_visual_form_add_scripts() {
-		if ( get_current_screen()->id === 'premios-inscricoes' ) {
-			wp_enqueue_style( 'bv-admin-css', FU_URL . 'lib/css/visual-form/style-admin.css' );
-			wp_enqueue_script( 'bv-admin-js', FU_URL . 'lib/js/visual-form/script-admin.js' );
+		if ( in_array( get_current_screen()->id, FU_WP_Visual_Form::fu_get_allowed_post_types() ) ) {
+			wp_enqueue_style( 'bv-admin-css', FU_URL . 'lib/css/fu-visual-form.css' );
+			//wp_enqueue_script( 'bv-admin-js', FU_URL . 'lib/js/visual-form/script-admin.js' );
 		}
 	}
 
-	/**
-	 * Displays visual form button on post editor
-	 */
-	public static function add_btn_fu_visual_form() {
-		$current = get_current_screen();
+	private static function fu_get_allowed_post_types() {
 		$arrDefaultPostTypesAllowed = array( 'post' );
 
-		$fuSettings                 = get_option( FU_WP_Visual_Form::settings_slug, FU_WP_Visual_Form::settings_defaults() );
+		$fuSettings = get_option( FU_WP_Visual_Form::settings_slug, FU_WP_Visual_Form::settings_defaults() );
 		if ( isset( $fuSettings['enabled_post_types'] ) ) {
 			$arrDefaultPostTypesAllowed = $fuSettings['enabled_post_types'];
 		}
@@ -41,11 +37,20 @@ class FU_WP_Visual_Form {
 		/**
 		 * Filter to customize button exhibition displaying on custom post types
 		 */
-		$arrPostTypesAllowed = apply_filters( 'fu_visual_form_button_allowed_post_types', $arrDefaultPostTypesAllowed );
-		if ( in_array( $current->id, $arrPostTypesAllowed, true ) ) {
+		return apply_filters( 'fu_visual_form_button_allowed_post_types', $arrDefaultPostTypesAllowed );
+	}
+
+	/**
+	 * Displays visual form button on post editor
+	 */
+	public static function add_btn_fu_visual_form() {
+		$current = get_current_screen();
+
+		if ( in_array( $current->id, FU_WP_Visual_Form::fu_get_allowed_post_types(), true ) ) {
 			?>
             <a href="/?TB_inline&inlineId=fu-btn-visual-form&width=500&height=400"
-               class="button add_media hide-if-no-js thickbox"><span class="dashicons dashicons-welcome-widgets-menus"></span><?php echo apply_filters('fu_visual_form_button_name', 'Add FU Form') ?></a>
+               class="button add_media hide-if-no-js thickbox"><span
+                        class="dashicons dashicons-welcome-widgets-menus"></span><?php echo apply_filters( 'fu_visual_form_button_name', 'Add FU Form' ) ?></a>
 
             <!--            <input type="hidden" id="abril-shortcode-btn-votar-finalista-id">-->
             <!--            <input type="hidden" id="abril-shortcode-btn-votar-premio-id">-->
@@ -53,24 +58,36 @@ class FU_WP_Visual_Form {
             <div id="fu-btn-visual-form" style="display:none;">
                 <div class="fu-btn-visual-form">
                     <div class="edit-shortcode-form">
-                        SCTDs
-
-                        fu-upload-form abre
-                        fu-upload-response
-
-                        input
-                        textarea
-                        select
-                        checkboxes
-                        radio
-                        recaptcha
-                        <!--                        <p>Preencha o texto que deseja exibir.</p>-->
-                        <!--                        <label>Texto do botão</label>-->
-                        <!--                        <input id="abril-shortcode-btn-votar-texto" class="abril-shortcode-btn-votar-texto" value="Gostou desta finalista?">-->
+                        <!--                        SCTDs-->
                         <!---->
-                        <!--                        <div class="abril-bv-btn-insert-container">-->
-                        <!--                            <button class="button button-primary button-large" id="abril-bv-inserir">Inserir Botão</button>-->
-                        <!--                        </div>-->
+                        <!--                        fu-upload-form abre-->
+                        <!--                        fu-upload-response-->
+                        <!---->
+                        <!--                        input-->
+                        <!--                        textarea-->
+                        <!--                        select-->
+                        <!--                        checkboxes-->
+                        <!--                        radio-->
+                        <!--                        recaptcha-->
+
+
+                        <div id="tab-container" class="tab-container">
+                            <ul class='etabs'>
+                                <li class='tab'><a href="#tabs-form">Formulário</a></li>
+                                <li class='tab'><a href="#tabs-grupos">Grupos</a></li>
+                            </ul>
+                            <div class='panel-container'>
+                                <div id="tabs-form">
+                                    <h2>HTML Markup for these tabs</h2>
+                                    <!-- content -->
+                                </div>
+                                <div id="tabs-grupos" style="display: none;">
+                                    <h2>JS for these tabs</h2>
+                                    <!-- content -->
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
